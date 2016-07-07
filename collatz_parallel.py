32edabb6657946b2e164f879cf1b3940a4565a81
Ian Giestas Pauli <iangiestaspauli@gmail.com>
2016-07-06 22:24:50 -0300

    Added new Collatz conjecture algorithm

---
+++ collatz_parallel.py
@@ -0,0 +1,32 @@
+import multiprocessing as mp
+
+NUMCPU = int(mp.cpu_count())
+
+def collatz(N):
+	c = N
+	while not c == 1:
+		if c % 2 == 0:
+			c = c / 2
+		else:
+			c = 3*c + 1
+	return True
+
+def main(my_n, total_num):
+	z = my_n
+	while True:
+		for n in xrange(10L**z,10**(z + 1)):
+			if collatz(n):
+				print n,"OK"
+			else:
+				print n,"NOK"
+				break
+		z += total_num
+	return False
+
+Threads = [mp.Process(target=main, args=(x,NUMCPU)) for x in range(NUMCPU)]
+for p in Threads:
+	p.start()
+
+# Exit the completed processes
+for p in processos:
+	p.join()
